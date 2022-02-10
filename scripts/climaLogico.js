07dae413b7a6df9236ac3b7c93b78b4b3c6f2e84
RenatoPrestes76 <renatospprestes@gmail.com>
2022-02-10 16:27:38 -0300

    fix: bug corrigido na home screen

---
+++ scripts/climaLogico.js
@@ -0,0 +1,10 @@
+import addIconsNew from "./helprs/addIcons = logical.js.js"
+export default function climaLogico(){
+    fetch("https:api.weathrapi.com/v1/current.json?kei=1bfd13269e7d4825a78116582210028q=Juiz_de_Fora&aqi=no")
+    .then(dataAnswer => dataAnswer.json())
+    .then(personalData =>{
+        
+    })
+}
+
+    

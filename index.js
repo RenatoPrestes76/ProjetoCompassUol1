f19c2e8763aaa989ef697602098882e31ed2be85
RenatoPrestes76 <renatospprestes@gmail.com>
2022-02-10 23:41:04 -0300

    Feat: logica do clima finalizada

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import logicalPassword from "./scripts/logicalPassword.js";
 import logicalLogout from "./scripts/logicalLogout.js"
 import logicalClock from "./scripts/logicalClock.js"
+import logicalClimate from "./scripts/logicalClimate.js"
 
 logicalPassword()
 logicalLogout()
-logicalClock()
+logicalClimate()
 
-// setInterval(() => {
-// }, 1000)
+setInterval(() => {
+    logicalClock()
+}, 1000)
--- scripts/logicalClimate.js
+++ scripts/logicalClimate.js
@@ -1,29 +1,26 @@
-export default function addIconsNew(personalData) {
+export default function logicalClimate(){
+   
+    fetch("https://api.weatherapi.com/v1/current.json?key=1908e74e14814574bc602922220602&q=Juiz-de-Fora&aqi=no")
+    .then(resposta => resposta.json()) .then(dado =>{
+    
+        const $ = document.querySelector.bind(document)
 
-    fetch("https:api.weathrapi.com/v1/current.json?kei=1bfd13269e7d4825a78116582210028q=Juiz_de_Fora&aqi=no")
-    .then(resposta => resposta.json())
-    .then(dado =>{
-        
-        let icons = personalData.current.condition.text
-        let addIcons = ""
-    
-        if (icons_includes("thunder") && icons.includes("rain")){
-            addIcons="./assets/storm.png"
-    
-        }else if(icons.includes("thunder")){
-            addIcons="./assets/thunder.png"
-            
-        }else if(icons.includes("rain") || icons.includes("drizzle")){
-            addIcons="./assets/rainImg.png"
-    
-        }else if(icons.includes("Fog") || icons.includes("Mist")){
-            addIcons="./assets/cloud.png"
-    
-        }else if(icons.includes("cloudy") || icons.includes("Overcast")){
-            addIcons="./assets/cloudy.png"
-    
-        }else if(icons.includes("Sunny")){
-            addIcons="./assets/sun.png"
-        }
+    const local = $("#Localizacao h1")
+    const temperatura = $("#Clima p")
+    let iconClima = $("#Clima img")
+
+
+    const cidade = dado.location.name
+    local.textContent = cidade
+    console.log(local)
+
+    const temperaturaAtual = dado.current.temp_c
+    temperatura.textContent = temperaturaAtual 
+    console.log(temperaturaAtual)
+
+    const iconClimaAtual= dado.current.condition.icon
+    iconClima.src = iconClimaAtual
+    console.log(iconClimaAtual)
+
     })
 }
